06e494f3354f18db4945623f7b22496043b192b3
suhara beevi <sairasuhara12345@gmail.com>
2024-02-23 11:13:00 +0530

    login form of client-side

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,15 +1,14 @@
-import styles from './signup.module.css'
+import styles from './style.module.css'
 import { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import {Link } from 'react-router-dom';
 import axios from 'axios'
 const Signup =()=>{
     const [data,setData] =useState({
-        username:"",
         email:"",
         password:""
     });
     const [error,setError] = useState("")
-    const navigate = useNavigate()
+    
     const handleChange =({currentTarget:input})=>{
 setData({...data,[input.name]:input.value})
     }
@@ -18,10 +17,10 @@ setData({...data,[input.name]:input.value})
     const handleSubmit =async(e)=>{
         e.preventDefault()
         try{
-          const url= "http://localhost:5000/api/user";
+          const url= "http://localhost:5000/api/login";
           const {data:res} =await axios.post(url,data)
-          navigate("/login")
-          console.log(res.message);
+          localStorage.setItem("token",res.data)
+          window.location="/"
         }catch(error){
 if(error.response && error.response.status>=400 && error.response.status<=500){
     setError(error.response.data.message)
@@ -29,28 +28,28 @@ if(error.response && error.response.status>=400 && error.response.status<=500){
         }
     }
     return(
-<div className={styles.signup_container}>
-    <div className={styles.signup_form_container}>
+<div className={styles.login_container}>
+    <div className={styles.login_form_container}>
         <div className={styles.left}>
-<h1>welcome back</h1>
-<Link to='/login'>
-    <button type='button' className={styles.white_btn}>
-        sign in
-
-    </button>
-</Link>
-        </div>
-        <div className={styles.right}>
-            <form className={styles.form_container}>
-                <h1>Create Account</h1>
-<input type="text" placeholder='username' name='username' onChange ={handleChange} value={data.username} required className={styles.input}/>
+        <form className={styles.form_container}>
+                <h1>Login to your Account</h1>
 <input type="email" placeholder='Email' name='email' onChange ={handleChange} value={data.email} required className={styles.input}/>
 <input type="password" placeholder='Password' name='password' onChange ={handleChange} value={data.password} required className={styles.input}/>
 {error && <div className={styles.error_msg}>{error}</div>}
 <button type='submit' className={styles.green_btn} onSubmit={handleSubmit}>
-    Sign Up
+    Sign IN
 </button>
             </form>
+        </div>
+        <div className={styles.right}>
+        <h1>New Here</h1>
+<Link to='/signup'>
+    <button type='button' className={styles.white_btn}>
+        sign Up
+
+    </button>
+</Link>
+            
         </div>
     </div>
 </div>
