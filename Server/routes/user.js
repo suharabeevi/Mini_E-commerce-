4688e079a19a2751d79ecfd4d07b222b3ed84a7a
suhara beevi <sairasuhara12345@gmail.com>
2024-02-27 11:12:08 +0530

    add envexample file

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usercontroller= require('../Controllers/usercontroller')
 const upload =require('../multer')
+
+
 router.post("/", usercontroller.usersignup);
 router.post("/login",usercontroller.userlogin)
 router.post('/prodcuts',upload,usercontroller.addproducts)
